1a4a56a77f0822537af8c54e5daa43bab0d94602
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-20 15:17:09 +0700

    isi array

--- ArrayStruct.cpp
+++ ArrayStruct.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int niali[] = {1,2,4,7};
+int nilai[] = {1,2,4,7};
 
 int nilaiDua[3] ={1,2,3};
 
 int nilaiDua[4];
+
+string nama[4];
+
+int main()
+{
+   cout << "Nilai array ke 3 = " << nilai[2] <<endl;
+   nilai[2] = 100;
+   cout << "Nilai array ke 3 = " << nilai[2] << endl;
+
+
+   for (int i = 0; i<4; i++)
+   {
+    cout << "Masukan nama ke " << i + 1 << " = ";
+    cin >> nama[i];
+   }
+
+   cout << endl;
+   cout << "Menampilkan Array Nama" << endl;
+   cout << endl;
+
+   for (int i =0; i < 4; i++)
+   {
+    cout << "Nama ke " << i + 1 << " = " << nama[i] << endl;
+   }
+}
