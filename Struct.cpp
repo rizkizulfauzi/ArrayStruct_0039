893ddd8303f888fe7f062a66922fac54fe23dbbd
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-20 15:38:48 +0700

    object struct

--- Struct.cpp
+++ Struct.cpp
@@ -8,3 +8,22 @@ struct Mahasiswa
     string alamat;
 };
 
+int main()
+{
+    Mahasiswa mhs;
+
+    cout << "Masukan nim =";
+    cin >> mhs.nim;
+    cout << "Masukan nama =";
+    cin >> mhs.nama;
+    cout << "Masukan alamat =";
+    cin >> mhs.alamat;
+
+    cout << endl;
+    cout << "Menampilkan Data " << endl;
+    cout << endl;
+
+    cout << "NIM =" << mhs.nim <<endl;
+    cout << "Nama =" << mhs.nama <<endl;
+    cout << "alamat" << mhs.alamat <<endl;
+}
