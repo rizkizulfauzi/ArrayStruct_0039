c7e4669b28c7a9e8478cdecc8f0c0dd35c818e9f
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-20 15:22:08 +0700

    Deklarasi2

--- NestedStruct.cpp
+++ NestedStruct.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 using namespace std;
 
+struct DetailAlamat
+{
+  string kota;
+  string provinsi;  
+};
+
+struct Mahasiswa
+{
+string nama;
+string nim;
+DetailAlamat alamat;
+};
