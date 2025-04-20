55f2608b2a7cfe426ab9cd736bc6c4d866c1733c
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-20 15:52:57 +0700

    deklarasi5

--- Struct_getline.cpp
+++ Struct_getline.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+struct DetailAlamat
+{
+    string kota;
+    string provinsi;
+};
+
+struct Mahasiswa
+{
+   string nama;
+   string nim;
+   DetailAlamat alamat;
+};
+int main(){}
+
