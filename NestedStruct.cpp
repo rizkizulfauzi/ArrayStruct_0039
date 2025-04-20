4d8c627dd2c9623a03313a36054f8a2a4cdb342d
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-20 15:32:31 +0700

    isi object struct

--- NestedStruct.cpp
+++ NestedStruct.cpp
@@ -13,3 +13,31 @@ string nama;
 string nim;
 DetailAlamat alamat;
 };
+
+int main()
+{
+    Mahasiswa mhs[12];
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukan nim-";
+        cin >> mhs[i].nim;
+        cout << "Masukan Nama =";
+        cin >> mhs[i].nama;
+        cout << "Masukan kotan =";
+        cin >> mhs[i].alamat.kota;
+        cout << "Masukan provinsi =";
+        cin >> mhs[i].alamat.provinsi;
+    }
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i +1 << endl;
+        cout << "NIM = " <<mhs[i].nim <<endl;
+        cout << "Nama =" <<mhs[i].nama <<endl;
+        cout << "kota =" <<mhs[i].alamat.kota;
+        cout << "Provinsi =" <<mhs[i].alamat.provinsi;
+    }
+}
