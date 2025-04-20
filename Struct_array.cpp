ca46790cad74651cc4968ade50d872f60fef0c0a
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-20 15:47:22 +0700

    object struct dlm array

--- Struct_array.cpp
+++ Struct_array.cpp
@@ -8,4 +8,27 @@ struct Mahasiswa
     string alamat;
 };
 
-int main(){}
+int main()
+{
+    Mahasiswa mhs[4];
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Mahasiswa ke-" << i +1 <, endl;
+        cout << "Masukan nim =";
+        cin >> mhs[i].nim;
+        cout << "Masukan alamat = ";
+        cin >> mhs[i].alamat;
+    }
+
+    cout << endl;
+    cout << "Menampilkan Data " << endl;
+    cout << endl;
+    for (int i =0; i <4; i++)
+    {
+        cout << " Mahasiswa ke- " << i +1 << endl;
+        cout << "NIM" << mhs[i].nim;
+        cout << "Nama" << mhs[i].nama;
+        cout << "Alamat" << mhs[i].alamat <<endl;
+
+    }
+}
